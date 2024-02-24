c0da1243b14c206686de9dee4fdabd3122bdeb07
Your felixaveiro <bikofelix2020@gmail.com>
2024-02-24 16:34:05 +0200

    trial learn

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
+import React,{useState} from'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,   Button } from 'react-native';
 
-export default function App() {
+
+  const App = () => {
+  const [name, setName] = useState('aveiro');
+  const [session, setSession] = useState({number : 6, title : 'state'});
+  const[current, setCurrent] = useState(true);
+  const onClickHandler = () => {
+    setName('Programming with aveiro');
+    setSession({number : 10, title : 'React Native'});
+    setCurrent(!current);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.body}>
+      <Text style={styles.text}>My name is :{name}</Text>
+      <Text style={styles.text}>this is session number :{session.number} and about {session.title}</Text>
+      <Text style={styles.text}>{current ? 'current session' : 'next session'}</Text>
+      <Button title='update state ' onPress={onClickHandler}></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 20,
+    color: '#000',
+    textAlign: 'center',
+  }
 });
+export default App;
