129093d39f2fded5f72084ff98f5f99a108538f3
Your felixaveiro <bikofelix2020@gmail.com>
2024-02-27 01:53:55 +0200

    input

--- App.js
+++ App.js
@@ -1,40 +1,19 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 const App = () => {
-  const [name, setName] = useState('aveiro');
-  const [session, setSession] = useState({ number: 6, title: 'state' });
-  const [current, setCurrent] = useState(true);
-
-  const onClickHandler = () => {
-    setName('Programming with aveiro');
-    setSession({ number: 10, title: 'React Native' });
-    setCurrent(!current);
-  }
-
-  const [Items, setItems] = useState([
-    { key: 1, item: 'item 1' },
-    { key: 2, item: 'item 2' },
-    { key: 3, item: 'item 3' },
-    { key: 4, item: 'item 4' },
-    { key: 5, item: 'item 5' },
-    { key: 6, item: 'item 6' },
-    { key: 7, item: 'item 7' },
-    { key: 8, item: 'item 8' },
-  ]);
+  const [name, setName] = useState('');
 
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>My name is: {name}</Text>
-      <Text style={styles.text}>This is session number: {session.number} and about {session.title}</Text>
-      <Text style={styles.text}>{current ? 'current session' : 'next session'}</Text>
-      <Button title='update state' onPress={onClickHandler}></Button>
-      {Items.map((item) => (
-        <View style={styles.item} key={item.key}>
-          <Text style={styles.text}>{item.item}</Text>
-        </View>
-      ))}
+      <Text style={styles.text}>Please write your name:</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+        placeholder="Enter your name"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -52,10 +31,13 @@ const styles = StyleSheet.create({
     color: '#000',
     textAlign: 'center',
   },
-  item: {
-    backgroundColor: '#4ae1fa',
-    padding: 10,
-    margin: 5,
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 10,
+    paddingHorizontal: 10,
   }
 });
 
