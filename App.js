9529a800f45838a852ed1a6259ddaa001b44bfe0
Your felixaveiro <bikofelix2020@gmail.com>
2024-02-27 01:01:21 +0200

    upadate

--- App.js
+++ App.js
@@ -12,12 +12,25 @@ import { StyleSheet, Text, View ,   Button } from 'react-native';
     setSession({number : 10, title : 'React Native'});
     setCurrent(!current);
   }
+  const [Items,setItems] = useState([
+    {key:1,item:'item 1'},
+    {key:2,item:'item 2'},
+    {key:3,item:'item 3'},
+    {key:4,item:'item 4'},
+    {key:5,item:'item 5'},
+    {key:6,item:'item 6'},
+    {key:7,item:'item 7'},
+    {key:8,item:'item 8'},
+  ])
   return (
     <View style={styles.body}>
       <Text style={styles.text}>My name is :{name}</Text>
       <Text style={styles.text}>this is session number :{session.number} and about {session.title}</Text>
       <Text style={styles.text}>{current ? 'current session' : 'next session'}</Text>
       <Button title='update state ' onPress={onClickHandler}></Button>
+      <view style ={styles.item}>
+        <Text  styles={styles.text}>Items</Text>
+      </view>
       <StatusBar style="auto" />
     </View>
   );
@@ -34,6 +47,10 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: '#000',
     textAlign: 'center',
+  },
+  item: {
+    backgroundColor: '#4ae1fa',
   }
 });
+
 export default App;
